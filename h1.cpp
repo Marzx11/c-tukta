08260e3c7c27e6db8e6fd67123bbda8272f6f74b
Marzx11 <natchamitmana019@gmail.com>
2021-10-21 01:25:52 +0700

    7-3 complete

--- h1.cpp
+++ h1.cpp
@@ -1,29 +1,18 @@
 #include<iostream>
 #include<string.h>
 #include <time.h>
+#include<algorithm>
 using namespace std;
 
 struct  Student {
 	string id;
 	string name;
-	float test[3], total; // test [4];
+	float test[3], total,avg; // test [4];
 };
-
 void inputData(Student stu[]);
-void displayData(Student stu[]);
 void sumTest(Student stu[]);
-void sortdata(struct Student stu[]);
-
-int main() {
-	int n = 5;
-	struct Student stu[5];
-	inputData(stu);
-	sumTest(stu);
-	sortdata(stu);
-	displayData(stu);
-	
-	return 0;
-}
+void sortdata(Student a[], int n);
+void displayData(Student stu[]);
 
 void inputData(Student stu[]) 
 {
@@ -55,37 +44,57 @@ void inputData(Student stu[])
 }
 void sumTest(Student stu[]) 
 {
-	for(int n = 0; n < 5; n++) {
+	for(int n = 0; n < 5; n++) 
+	{
 		stu[n].total = 0.0f;
-		for(int m = 0; m < 3; m++) {
-			stu[n].total = stu[n].total + stu[n].test[m];
+		for(int m = 0; m < 3; m++) 
+		{
+			stu[n].total += stu[n].test[m];
 		}
 	}
 }
-
-void sortdata(Student stu[])
+bool compareTwoStudents(Student a, Student b)
 {
-	int i,j;
-	struct Student temp;
-	for (i = 1; i < 5-1; i++)
-    {
-        for (j = i+1; j < 5; j++)
-        {
-            if (stu[j].total < stu[j].total)
-            {
-                temp = stu[i];
-                stu[i] = stu[j];
-                stu[j] = temp;
-            } 
-        }
-    }
+    // If total marks are not same then
+    // returns true for higher total
+    if (a.total != b.total)
+        return a.total > b.total;
 }
-void displayData(Student stu[]) {
+void sortdata(Student a[], int n)
+{
+    sort(a, a + 5, compareTwoStudents);
+}
+
+void displayData(Student stu[])
+{
+	float w;
+	for(int i=0;i<5;i++){
+		w += stu[i].total;
+		stu[i].avg = 0.0f;
+		for(int j=0;j<3;j++)
+			stu[j].avg += stu[i].test[j];
+	}
+	cout << " ----------------------------------------------------------------------------------" << endl;
+    cout << "  NO.    ID       NAME          Test1(25%)    Test2(25%)    Test3(50%)   Total(100%)" << endl;
+    cout << " ----------------------------------------------------------------------------------" << endl;
 	for(int n = 0; n < 5; n++) {
-		cout << stu[n].id << "\t" << stu[n].name << "\t";
+		cout << n+1 << "\t" << stu[n].id << "\t" << stu[n].name << "\t";
 		for(int m = 0; m < 3; m++) {
 			cout << stu[n].test[m] << "\t";
 		}
 		cout << stu[n].total  << endl;
 	}
+	cout << " ----------------------------------------------------------------------------------" << endl;
+	cout << "Average of mark " << stu[0].avg/5 <<"\t" << stu[1].avg/5 <<"\t" << stu[2].avg/5  <<"\t" << w/5 <<"\t";
+}
+
+int main() {
+	int n = 5;
+	struct Student s[5];
+	struct Student stu[5];
+	inputData(stu);
+	sumTest(stu);
+	sortdata(stu,n);
+	displayData(stu);
+	return(0);
 }
